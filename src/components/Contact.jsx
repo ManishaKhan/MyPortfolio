fd7e9d86b078dbc6f0d9d8626d1339f46809c406
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2026-07-24 18:08:37 +0530

    change cv name

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -202,7 +202,7 @@ export default function Contact() {
 
           <Button
             component="a"
-            href="/Manisha_Khan_Resume.pdf"
+            href="/Manisha_Khan_CV.pdf"
             target="_blank"
             rel="noopener noreferrer"
             variant="outlined"
