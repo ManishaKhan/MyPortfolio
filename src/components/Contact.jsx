db0c2165e8f25d05876c5888012bf7ef89873d5d
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2026-07-24 19:22:52 +0530

    update cv text

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -202,7 +202,7 @@ export default function Contact() {
 
           <Button
             component="a"
-            href="/Manisha_Khan_CV.pdf"
+            href="/Manisha_Khan_UpdatedCV.pdf"
             target="_blank"
             rel="noopener noreferrer"
             variant="outlined"
